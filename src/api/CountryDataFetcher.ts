74b49ccb6070fa7f4c47d65167f3137e041ae358
Gagan Mani <gaganmani90@gmail.com>
2023-09-05 17:40:45 -0700

    add cors on country api

--- src/api/CountryDataFetcher.ts
+++ src/api/CountryDataFetcher.ts
@@ -3,9 +3,15 @@ import Country from '../pojo/Country'; // Import the Country type
 
 export const fetchCountryData = async (): Promise<Country[]> => {
     try {
-        const response = await axios.get('/countries/?format=json&page=1&per_page=1000');
-        const data: Country[] = response.data[1];
-        return data;
+        // Set CORS headers in the request
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+            },
+        };
+        const response = await axios.get('/countries/?format=json&page=1&per_page=1000', config);
+        return response.data[1];
     } catch (error) {
         console.error('Error fetching country data:', error);
         return [];
